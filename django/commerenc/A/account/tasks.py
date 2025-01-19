06de22f3492657ee973ae2e8b2a5f85e8027d198
erfan <computerprogrammeralexsafar@gmail.com>
2025-01-20 00:50:35 +0330

    mongard laptop needed transfared

--- django/commerenc/A/account/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from .models import OTPCode
-from datetime import datetime , timedelta
-import pytz
-@shared_task()
-def remove_expired_otp_codes() :
-    expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
-    OTPCode.objects.filter(created__lt=expired_time).delete()
